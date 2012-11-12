54dbfbee3efa0253a15d1e9eeceb6576f5fa5551
SlowFrog <laurentvaucher@gmail.com>
2012-11-12 20:12:20 +0100

    Full cleanup of main.js

--- dominotrail/main.js
+++ dominotrail/main.js
@@ -7,24 +7,6 @@ dt.start = function() {
   util.init();
 
   var game = new dt.Game();
-  game.addObserver({
-    update: function(event) {
-      util.log("Game update", event);
-    }
-  });
-
   var view = new dt.View(document.getElementById("gamearea"), game);
   var controller = new dt.Controller(view, game);
-};
-
-dt.onClick = function(event, view, ctx, game) {
-  if (event.button == 0) { // Left-click
-    
-    var action = branchoff.mode;
-
-    var x = event.clientX + util.windowScrollX() - view.x0;
-    var y = event.clientY + util.windowScrollY() - view.y0;
-    
-    //
-  }
 };
