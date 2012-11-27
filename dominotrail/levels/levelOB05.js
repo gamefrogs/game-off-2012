d91463ac328ab254b0908b3ad8d5015e40e1b6a3
oBiB <olivier.bibard@laposte.net>
2012-11-27 20:14:32 +0100

    Add level OB05

---
+++ dominotrail/levels/levelOB05.js
@@ -0,0 +1,46 @@
+dt.LEVELOB05_STR = 
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  ". 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .    \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  "  . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .  \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  ". 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .    \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  "  . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .  \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  ". 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .    \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  "  . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .  \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  ". 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .    \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  "  . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .  \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  ". 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .    \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  "  . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .  \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  ". 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .    \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  "  . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .  \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     \n" +
+  ". 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 . 1 .    \n" +
+  " . . . . . . . . . . . . . . . . . . . . . .     ";
+dt.LEVELOB05 =
+  new dt.LevelDef("OB 5", 11, 13, dt.LEVELOB05_STR,
+                  [
+                   { x: 2, y: 3, type: dt.StraightEndPiece, dir: dt.Dir.SE, goal: true, beginAct:15, endAct:16 },
+                   { x: 7, y: 6, type: dt.StraightStartPiece, dir: dt.Dir.E, goal: false },
+                   { x: 2, y: 9, type: dt.StraightEndPiece, dir: dt.Dir.NE, goal: true, beginAct:14, endAct:15 },
+                  ],
+                  [
+                   { type: dt.StraightDominoPiece, limit: Infinity },
+                   { type: dt.TurnRightDominoPiece, limit: Infinity },
+                   { type: dt.TurnLeftDominoPiece, limit: Infinity },
+                   { type: dt.RForkDominoPiece, limit: Infinity },
+                   { type: dt.LForkDominoPiece, limit: Infinity },
+                   { type: dt.ForkDominoPiece, limit: Infinity },
+                   { type: dt.TriForkDominoPiece, limit: Infinity },
+                  ]);
+// Register level
+dt.LEVELS.push(dt.LEVELOB05);
